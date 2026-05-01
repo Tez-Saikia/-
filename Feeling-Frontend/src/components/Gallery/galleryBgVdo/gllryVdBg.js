0ad47405bca58747ab46edcaa69d8ed6b4e4bb31
Tez-Saikia <tezsaikia14@gmail.com>
2026-05-01 12:56:02 +0530

    updated the video file

--- Feeling-Frontend/src/components/Gallery/galleryBgVdo/gllryVdBg.js
+++ Feeling-Frontend/src/components/Gallery/galleryBgVdo/gllryVdBg.js
@@ -1,7 +1,7 @@
-import gllryBgVd from "./gllryBgVd.mp4";
 import Rose_bg from "./RoseBg.png"
 
 export const BgVd = {
-    gllryBgVd,
+    gllryBgVd:
+    "https://res.cloudinary.com/disnhsvts/video/upload/q_auto,f_auto/v1777619985/gllryBgVd_f6himf.mp4",
     Rose_bg
 }
